2e1d520883f7ae5b74995775709c59065a6049e9
Zahra Motala <zahra.motala04@gmail.com>
2025-12-03 19:34:36 -0500

    Initial Node API

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use("/api/books", require("./routes/BookRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
-    
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
